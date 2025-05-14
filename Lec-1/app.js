d38a473e1bb9b4e5221ea395a17561c0dc66977e
aditya25122005 <aditya25pda@gmail.com>
2025-05-14 23:43:36 +0530

    Lec-1

---
+++ Lec-1/app.js
@@ -0,0 +1,24 @@
+let fullName="Aditya\nmaheshwari"
+console.log(fullName);
+
+let firstname="Aditya"
+let lastname="Maheshwari";
+let greet=`Hi ${firstname} ${lastname} how are You`
+console.log(greet);
+
+
+let num1=3;
+let num2=5;
+console.log(num2);
+
+console.log(num2/num1);
+console.log(num1/num2);
+console.log(num1%num2);
+console.log(3**2);
+console.log(3*5);
+
+
+
+
+
+
