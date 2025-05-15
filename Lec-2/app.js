f0c3348cb98e9fcdb4eb2ad5768f79c40338b33b
aditya25122005 <aditya25pda@gmail.com>
2025-05-16 00:42:25 +0530

    Lec-2

---
+++ Lec-2/app.js
@@ -0,0 +1,106 @@
+// let a=Number.MAX_SAFE_INTEGER;
+// let b=Number.MIN_SAFE_INTEGER;
+// console.log(a);
+// console.log(b);
+
+// let str='Hey! I am lookinf for a random awesome project, thankYou';
+// console.log(str.charAt(3));
+// console.log(str[3]);
+
+// let magicNumber=10;
+// console.log(magicNumber);
+// console.log(typeof(magicNumber));
+// magicNumber='sam';
+
+// let naam=10;
+// naam=true;
+// console.log(naam);
+// naam='adi';
+// console.log(naam);
+
+
+// let magic=10;
+// magic ='I am learning JS';
+// console.log(magic[100]);
+
+// let value;
+// console.log(value);
+
+// let num;
+// num=25;
+// console.log(num);
+
+
+
+
+
+
+// let str;
+// str='aditya maheshwari';
+// // console.log(str);
+
+// console.log(str.substr(1,3)) // dit
+// console.log(str.substr(2));  //ity---->
+// console.log(str.substr(-5,19));
+
+
+// console.log(str.substring(3));  //tya maheshwari
+// console.log(str.substring(2,5));
+// console.log(str.substring(5,2));
+
+// console.log(str.substring(-5,2));   //-5 will change to 0 (0,2)
+// console.log(str.substring(-5,-2));
+
+
+let s='Aditya New Erama';
+// console.log(s.indexOf('a'));
+// console.log(s.indexOf('a',6));
+// // console.log(s.indexOf(a,2));
+
+// console.log(s.indexOf('a'));
+// console.log(s.indexOf('a',s.indexOf('a')+1));
+
+// console.log(s.indexOf('a',s.indexOf('a',s.indexOf('a')+1)+1));
+
+
+
+let s2=s.replace('dit','nan');
+console.log(s2);
+
+
+let s3=s.replace('i','u');
+console.log(s3);
+
+let s4=s.replaceAll('a','**');
+console.log(s4);
+
+console.log(s4.repeat(3));
+
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+
+s="   **AAALLLLL**  ";
+console.log(s.trim());
+
+
+
+
+// let boolVariable=true;
+// console.log(boolVariable);
+// let newBool=false;
+// let one='true';
+// console.log(boolVariable==one);
+let boolvariable=true;
+let newBool=false;
+let one='true';
+let two='false';
+let ans1=(boolvariable===newBool);
+console.log(ans1);
+
+console.log(two==newBool);
+
+let ans3= (boolvariable==1);
+console.log(ans3);
+
+let ans4=(boolvariable===1);
+console.log(ans4);
